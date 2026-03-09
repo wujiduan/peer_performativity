8bd9d666c191e30a930f2c123cc6cfadc3cd5433
wujiduan <51787079+wujiduan@users.noreply.github.com>
2026-03-09 15:01:08 +0100

    Update pokec_simulations.py

--- pokec_simulations.py
+++ pokec_simulations.py
@@ -98,60 +98,9 @@ numerical_features = [
     ]
 categorical_features = [
     "gender",
-    # "region"
     ]
 textual_features = [
-    # "body",
-    # "I_am_working_in_field",
-    # "spoken_languages",
-    # "hobbies",
-    # "I_most_enjoy_good_food",
-    # "pets",
-    # "body_type",
-    # "my_eyesight",
-    # "eye_color",
-    # "hair_color",
-    # "hair_type",
-    # "completed_level_of_education",
-    # "favourite_color",
-    # "relation_to_smoking",
     "relation_to_alcohol",
-    # "sign_in_zodiac",
-    # "on_pokec_i_am_looking_for",
-    # "love_is_for_me",
-    # "relation_to_casual_sex",
-    # "my_partner_should_be",
-    # "marital_status",
-    # "children",
-    # "relation_to_children",
-    # "I_like_movies",
-    # "I_like_watching_movie",
-    # "I_like_music",
-    # "I_mostly_like_listening_to_music",
-    # "the_idea_of_good_evening",
-    # "I_like_specialties_from_kitchen",
-    # "fun",
-    # "I_am_going_to_concerts",
-    # "my_active_sports",
-    # "my_passive_sports",
-    # "profession",
-    # "I_like_books"
-    # "life_style",
-    # "music",
-    # "cars",
-    # "politics",
-    # "relationships",
-    # "art_culture",
-    # "hobbies_interests",
-    # "science_technologies",
-    # "computers_internet",
-    # "education",
-    # "sport",
-    # "movies",
-    # "travelling",
-    # "health",
-    # "companies_brands",
-    # "more"
 ]
 
 
@@ -231,7 +180,6 @@ def preprocess(df, target_column, edge_path="pokec_dataset/relationships.txt"):
     df_public_ids = set(df_public["user_id"].values)
     edges = pd.read_csv(edge_path, sep="\t", header=None)
     network_g = nx.Graph()
-    # print("number of users with public friendships:", len(df_public_ids))
     for edge in edges.itertuples():
         if edge[1] not in df_public_ids or edge[2] not in df_public_ids:
             continue 
